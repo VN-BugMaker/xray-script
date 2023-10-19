5d9529374745a3141515d5e262e9dccaa1412f4f
VN-BugMaker <137518613+VN-BugMaker@users.noreply.github.com>
2023-10-19 15:08:38 +0700

    Update install_xray_reality.sh

--- install_xray_reality.sh
+++ install_xray_reality.sh
@@ -17,8 +17,8 @@ ln -s /snap/bin/certbot /usr/bin/certbot
 certbot certonly --standalone --register-unsafely-without-email -d $domain
 
 # Copy SSL certificate files
-cp /etc/letsencrypt/archive/*/fullchain*.pem /etc/ssl/private/fullchain.cer
-cp /etc/letsencrypt/archive/*/privkey*.pem /etc/ssl/private/private.key
+cp /etc/letsencrypt/archive/$domain/fullchain*.pem /etc/ssl/private/fullchain.cer
+cp /etc/letsencrypt/archive/$domain/privkey*.pem /etc/ssl/private/private.key
 chown -R nobody:nogroup /etc/ssl/private
 chmod -R 0644 /etc/ssl/private/*
 
@@ -27,8 +27,8 @@ printf "0 0 1 * * /root/update_certbot.sh\n" > update && crontab update && rm up
 cat > /root/update_certbot.sh << EOF
 #!/usr/bin/env bash
 certbot renew --pre-hook "systemctl stop nginx" --post-hook "systemctl start nginx"
-cp /etc/letsencrypt/archive/*/fullchain*.pem /etc/ssl/private/fullchain.cer
-cp /etc/letsencrypt/archive/*/privkey*.pem /etc/ssl/private/private.key
+cp /etc/letsencrypt/archive/$domain/fullchain*.pem /etc/ssl/private/fullchain.cer
+cp /etc/letsencrypt/archive/$domain/privkey*.pem /etc/ssl/private/private.key
 EOF
 chmod +x update_certbot.sh
 
